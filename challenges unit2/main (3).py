0e0ed42ea2418973e1ffde8c06c759833530d67e
Thilakesh1 <144645237+Thilakesh1@users.noreply.github.com>
2023-09-11 20:23:24 +0530

    Add files via upload

---
+++ challenges unit2/main (3).py
@@ -0,0 +1,34 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please deposit a positive amount.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
+
+    def display_balance(self):
+        print(f"Account Holder: {self.__account_holder_name}")
+        print(f"Account Number: {self.__account_number}")
+        print(f"Account Balance: ${self.__account_balance}")
+
+
+# Creating an instance of the BankAccount class
+my_account = BankAccount("1234567890", "John Doe", 1000.0)
+
+# Testing deposit and withdrawal
+my_account.display_balance()
+my_account.deposit(500)
+my_account.withdraw(200)
+my_account.display_balance()
